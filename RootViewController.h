b56553d27f475ec8694df8abd74c45600ce230f7
jscz3w <jscz3w@126.com>
2015-09-23 19:22:12 +0800

    UPDATA

--- RootViewController.h
+++ RootViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ScannerViewController.h"
 
-@interface RootViewController : UIViewController
+@interface RootViewController : UIViewController<ScannerViewControllerDelegate>
 
 @end
--- RootViewController.m
+++ RootViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "RootViewController.h"
+#import "ScannerViewController.h"
 
 @interface RootViewController ()
-
+@property (nonatomic,strong)UITextView * txtView ;
+@property (nonatomic,strong)UILabel * txtLabel;
 @end
 
 @implementation RootViewController
@@ -18,8 +20,75 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UIColor *aColor = [UIColor blueColor];
-    self.view.backgroundColor=aColor;
+    UIImage *bkImg =[UIImage imageNamed:@"background@2x"];
+    NSLog(@"bkImg size= %d",(int)bkImg.size.width);
+    UIImageView *bkView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+    [bkView setImage:bkImg];
+    [self.view addSubview:bkView];
+    
+    
+    UIImage *imgScanBtn =  [UIImage imageNamed:@"scanBtn@2x"];
+    UIImage *imgAceessBtn =  [UIImage imageNamed:@"aceessBtn@2x"];
+    
+    UIButton * scanBtn = [[UIButton alloc]initWithFrame:CGRectMake(50, self.view.bounds.size.height-90, 95, 36)];
+    [scanBtn setBackgroundImage:imgScanBtn forState:UIControlStateNormal];
+    [scanBtn addTarget:self action:@selector(scanBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:scanBtn];
+    
+    
+    
+    UIButton * aceessBtn = [[UIButton alloc]initWithFrame:CGRectMake(self.view.bounds.size.width-50-95, self.view.bounds.size.height-90, 95, 36)];
+    [aceessBtn setBackgroundImage:imgAceessBtn forState:UIControlStateNormal];
+    [self.view addSubview:aceessBtn];
+    [aceessBtn addTarget:self
+                  action:@selector(accessBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    //UIImage *txtImg = [UIImage imageNamed:@"txtImg@2x"];
+    //NSLog(@"txtImg size= %d",(int)txtImg.size.width);
+    
+   
+    //[self.view addSubview:bktxtView];
+    
+    
+    // _txtView = [[UITextView alloc]initWithFrame:CGRectMake(16, self.view.bounds.size.height-310-108, self.view.bounds.size.width-32, 310)];
+    
+    //[txtView setBackgroundColor:[UIColor colorWithPatternImage:txtImg]];
+    
+    //UIImageView *bktxtView = [[UIImageView alloc]initWithFrame:CGRectMake(-2, -2, txtView.bounds.size.width, txtView.bounds.size.height)];
+    //[bktxtView setImage:txtImg];
+    
+    //[txtView addSubview:bktxtView];
+    
+   // _txtView.backgroundColor =[UIColor clearColor];
+    
+    //[self.view addSubview:_txtView];
+    _txtLabel = [[UILabel alloc]initWithFrame:CGRectMake(16, self.view.bounds.size.height-310-108, self.view.bounds.size.width-32, 310)];
+    _txtLabel.numberOfLines=0;
+    [self.view addSubview:_txtLabel];
+    
+   // UIColor *aColor = [UIColor colorWithPatternImage:bkImg];
+   // self.view.backgroundColor=aColor;
+    
+}
+
+-(void)accessBtnClick{
+    
+    NSURL* MISurl = [[ NSURL alloc ] initWithString :_txtLabel.text];
+    [[UIApplication sharedApplication ] openURL:MISurl];
+}
+
+-(void)scanBtnClick{
+
+        ScannerViewController *svc=[[ScannerViewController alloc]init];
+        svc.delegate=self;
+        [self presentViewController:svc animated:YES completion:nil];
+    
+    
+}
+
+-(void)scanResult:(NSString *)result{
+    self.txtLabel.text=result;
 }
 
 - (void)didReceiveMemoryWarning {
